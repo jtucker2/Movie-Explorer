010db59b28a563050e21039a0b79842617e4943f
Josh Tucker <joshuatucker648@gmail.com>
2023-10-31 00:04:58 +0000

    Migrated to typescript

---
+++ src/Item.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { Config } from './apiInterfaces';
+// import posterholder from '../assets/posterholder.png';
+
+function titleLengthLimiter(title: string) {
+  if (title.length > 19) {
+    title = title.slice(0, 18) + "..."
+  }
+  return title;
+}
+
+interface ItemProps {
+  config: Config;
+  // enterMoviePage: () => void;
+  // selectMovie: () => void;
+}
+
+export default function Item({config}: ItemProps) {
+  // const [poster, setPoster] = useState(config.images.poster)
+
+  // if (poster == "") setPoster(posterholder);
+
+  return (
+    <div className="item">
+      <img src={"https://image.tmdb.org/t/p/w500" + config.poster_path} id="list-poster" draggable="false"/>
+      <div id="list-title">{titleLengthLimiter(config.title)}</div>
+      <div id="list-year">{config.release_date}</div>
+    </div>
+  )
+}
